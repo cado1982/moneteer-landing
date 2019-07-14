0db6636944cf2005ae0d5308e7fd69772e54ec0b
Chris Oliver <cado82@gmail.com>
2019-07-14 22:25:47 +0100

    Add health check

---
+++ Moneteer.Landing/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moneteer.Landing.V2.Controllers
+{
+    [AllowAnonymous]
+    public class HealthCheckController : Controller
+    {
+        [Route("healthcheck")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+    }
+}
