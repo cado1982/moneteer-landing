e5306629be52ec80e8a19b7f897ed4dd1d8030b8
Chris Oliver <cado82@gmail.com>
2019-07-24 13:11:57 +0100

    Fix cookie consent

--- app/Moneteer.Landing/Startup.cs
+++ app/Moneteer.Landing/Startup.cs
@@ -123,15 +123,15 @@ namespace Moneteer.Landing.V2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseDatabaseErrorPage();
-            // }
-            // else
-            // {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
-            // }
+            }
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
