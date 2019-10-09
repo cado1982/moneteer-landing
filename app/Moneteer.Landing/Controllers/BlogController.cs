b895e8d0ba754b32e6d55191f30f6801b9b216a6
Chris Oliver <cado82@gmail.com>
2019-10-09 15:34:03 +0100

    Add philosophy page

---
+++ app/Moneteer.Landing/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moneteer.Landing.V2.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
