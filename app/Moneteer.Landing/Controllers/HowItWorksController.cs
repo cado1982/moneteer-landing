218198f1a8fdb7db08b1c5a0813d4944c8e8a263
Chris Oliver <cado82@gmail.com>
2019-10-11 14:39:57 +0100

    Add How it works section

---
+++ app/Moneteer.Landing/Controllers/HowItWorksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moneteer.Landing.V2.Controllers
+{
+    public class HowItWorksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
